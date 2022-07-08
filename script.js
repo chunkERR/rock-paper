29c7650fd06d5088e7716edec909c90817cf5be9
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2022-07-08 13:48:02 +0200

    further changes

--- script.js
+++ script.js
@@ -1,40 +1,92 @@
 let computerMove;
 
+let roundResult = '';
+
+let playerScore = 0;
+let computerScore = 0;
+let score = `The current score is ${playerScore} : ${computerScore}`;
+
+
 function computerPlay(min, max) {
-  const randInt = Math.floor(Math.random() * (max - min) + min);
-  if (randInt == 1) {
+  computerMove = Math.floor(Math.random() * (4 - 1) + 1);
+  if (computerMove == 1) {
     computerMove = "rock";
-  } else if (randInt == 2) {
+  } else if (computerMove == 2) {
     computerMove = "scissors";
-  } else if (randInt == 3) {
+  } else if (computerMove == 3) {
     computerMove = "paper";
   }
   return computerMove;
 }
 
-function playRound(playerSelection, computerSelection) {
-  computerPlay(1, 4);
-  let playerMove = prompt("What is your move? Rock? Paper? Scissors?");
-  let lower = playerMove.toLowerCase();
-  if (playerSelection == "rock" && computerSelection == "scissors") {
-    console.log("You win! Rock beats scissors.");
-  } else if (playerSelection == "rock" && computerSelection == "paper") {
-    console.log("You lose! Paper beats rock.");
-  } else if (playerSelection == "rock" && computerSelection == "rock") {
-    console.log("Draw! No one wins!.");
-  } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    console.log("You lose! Scissors beat paper.");
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
-    console.log("You win! Paper beats rock.");
-  } else if (playerSelection == "paper" && computerSelection == "paper") {
-    console.log("Draw! No one wins!.");
-  } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-    console.log("Draw! No one wins!.");
-  } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    console.log("You lose! Rock beat scissors.");
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    console.log("You win! Scissors beat paper.");
+
+// let playerMove = prompt("What is your move? Rock? Paper? Scissors?");
+
+// function playRound(playerSelection, computerSelection) {
+//   computerSelection = computerPlay();
+//   if (playerSelection == "rock" && computerSelection == "scissors") {
+//     return (`You win! ${playerSelection} beats ${computerSelection}.`);
+//   } else if (playerSelection == "rock" && computerSelection == "paper") {
+//     return ("You lose! Paper beats rock.");
+//   } else if (playerSelection == "rock" && computerSelection == "rock") {
+//     return ("Draw! No one wins!.");
+//   } else if (playerSelection == "paper" && computerSelection == "scissors") {
+//     return ("You lose! Scissors beat paper.");
+//   } else if (playerSelection == "paper" && computerSelection == "rock") {
+//     return ("You win! Paper beats rock.");
+//   } else if (playerSelection == "paper" && computerSelection == "paper") {
+//     return ("Draw! No one wins!.");
+//   } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+//     return ("Draw! No one wins!.");
+//   } else if (playerSelection == "scissors" && computerSelection == "rock") {
+//     return ("You lose! Rock beat scissors.");
+//   } else if (playerSelection == "scissors" && computerSelection == "paper") {
+//     return ("You win! Scissors beat paper.");
+//   }
+// }
+
+
+
+
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    function playRound(playerSelection, computerSelection) {
+      computerSelection = computerPlay();
+      if (playerSelection == "rock" && computerSelection == "scissors") {
+        roundResult = ((`You win! ${playerSelection} beats ${computerSelection}.`));
+        playerScore++
+      } else if (playerSelection == "rock" && computerSelection == "paper") {
+        roundResult = ("You lose! Paper beats rock.");
+        computerScore++
+      } else if (playerSelection == "rock" && computerSelection == "rock") {
+        roundResult = ("Draw! No one wins!.");
+      } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        roundResult = ("You lose! Scissors beat paper.");
+        computerScore++;
+      } else if (playerSelection == "paper" && computerSelection == "rock") {
+        roundResult = ("You win! Paper beats rock.");
+        playerScore++;
+      } else if (playerSelection == "paper" && computerSelection == "paper") {
+        roundResult = ("Draw! No one wins!.");
+      } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        roundResult = ("Draw! No one wins!.");
+      } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        roundResult = ("You lose! Rock beat scissors.");
+        computerScore++;
+      } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        roundResult = ("You win! Scissors beat paper.");
+        playerScore++
+        return roundResult;
+      }
+      i++;
+
+    }
+    return score;
   }
 }
 
-console.log(playRound("rock", computerPlay(1, 4)));
+
+// let selection = prompt("What is your move? Rock? Paper? Scissors?");
+const computerSelection = computerPlay();
+console.log(game());
